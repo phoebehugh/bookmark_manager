506a0675edd765c1ad7db07d6b3402987fb5b80c
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-08 17:35:21 +0100

    User gets a password error when they do not match

--- Gemfile
+++ Gemfile
@@ -18,4 +18,5 @@ group :development, :test do
   gem 'dm-postgres-adapter'
   gem 'database_cleaner'
   gem 'bcrypt-ruby'
+  gem 'rack-flash3'
 end
--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,7 @@
 require 'data_mapper'
 require 'sinatra/base'
+require 'rack-flash'
+
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
@@ -15,6 +17,7 @@ class BookmarkManager < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
   enable :sessions
+  use Rack::Flash
   set :session_secret, 'super secret'
 
   get '/' do
@@ -39,15 +42,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
+    @user = User.create(email: params[:email],
+                        password: params[:password],
+                        password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash[:notice] = 'Sorry, your passwords do not match'
+      erb :'users/new'
+    end
   end
 
   helpers do
