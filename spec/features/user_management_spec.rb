f70b1e5224197927ae3427d7ccebe922bcc9870d
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-08 17:01:52 +0100

    adds password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 feature 'User signs up' do
   scenario 'when being a new user visiting the site' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, phoebecnh@yahoo.co.uk')
-    expect(User.first.email).to eq('phoebecnh@yahoo.co.uk')
+    expect(page).to have_content('Welcome, a@a.com')
+    expect(User.first.email).to eq('a@a.com')
   end
 
-  def sign_up(email = 'phoebecnh@yahoo.co.uk',
-              password = 'password1')
+  scenario 'with a password that does not match' do
+    expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+  end
+
+  def sign_up(email = 'a@a.com',
+              password = 'pass',
+              password_confirmation = 'pass')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
 end
