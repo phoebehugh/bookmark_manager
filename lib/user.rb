849c8a0dd0b17c624bae3c8c7d57ce524b66d727
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-08 19:10:43 +0100

    Errors work on an individual basis

--- lib/user.rb
+++ lib/user.rb
@@ -5,11 +5,11 @@ class User
 
   attr_reader :password
   attr_accessor :password_confirmation
-  validates_confirmation_of :password
+  validates_confirmation_of :password, message: 'Sorry, your passwords do not match'
   validates_uniqueness_of :email
 
   property :id, Serial
-  property :email, String, unique: true
+  property :email, String, unique: true, message: 'This email is already taken'
   property :password_digest, Text
 
   def password=(password)
