06dacd2009d796fa42b15cd509b008ed6d69d9b7
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-08 11:43:25 +0100

    adds tags functionailty

--- lib/server.rb
+++ lib/server.rb
@@ -4,6 +4,7 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
+require './lib/tag'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
@@ -16,10 +17,19 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
-  post 'links' do
+  post '/links' do
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(:text => params[:text])
+    @links = tag ? tag.links : []
+    erb :index
   end
 end
